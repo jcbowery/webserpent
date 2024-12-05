43ddc839b6067ee7ba53287e0a4f4f8fff0d5da7
Josh <josh@localhost.localdomain>
2024-12-05 16:40:25 -0500

    init

---
+++ tests/unit/test_demo.py
@@ -0,0 +1,4 @@
+from webserpent.demo import hello
+
+def test_demo():
+    assert hello() == 'world'
---
+++ webserpent/demo.py
@@ -0,0 +1,10 @@
+"""_summary_
+"""
+
+def hello():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+    return "world"
